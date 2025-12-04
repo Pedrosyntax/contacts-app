1a0ad0eca1270f7a99f2919cd0ab24b04e03010d
Pedrosyntax <22415147@bucks.ac.uk>
2025-12-04 13:03:02 +0000

    Tidy up code spacing for improved readability

--- tests.py
+++ tests.py
@@ -13,9 +13,16 @@ def run_tests():
     contacts_index.add_contact(contact3)
 
     print("Find by phone 12345:", contacts_index.find_by_phone("12345").full_name)
+
     print("Exact search 'Pedro:'", [c.full_name for c in contacts_index.find_by_name("Pedro")])
+
     print("Prefix search 'Pe':", [c.full_name for c in contacts_index.search_by_prefix("Pe")])
+
     print("All contacts sorted by name:", [c.full_name for c in contacts_index.all_sorted_by_name()])
+
+    contact_dup = Contact("Fred", "12345")
+    contacts_index.add_contact(contact_dup)
+            
 
 if __name__ == "__main__":
     run_tests()
