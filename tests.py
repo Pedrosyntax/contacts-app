b5e38b973c736a07d19cff0d294f0bb84d874f0f
Pedrosyntax <22415147@bucks.ac.uk>
2025-12-08 20:32:11 +0000

    remove contact and empty search test

--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ def run_tests():
     contact1 = Contact("Pedro", "12345")
     contact2 = Contact("Maria", "56789")
     contact3 = Contact("Belle", "54321")
+    contact4 = Contact("Mylo", "676767")
     contacts_index.add_contact(contact1)
     contacts_index.add_contact(contact2)
     contacts_index.add_contact(contact3)
@@ -22,7 +23,13 @@ def run_tests():
 
     contact_dup = Contact("Fred", "12345")
     contacts_index.add_contact(contact_dup)
-            
+
+    empty_search = contacts_index.find_by_name("NonExistent")
+    print("Empty search result:", empty_search)
+
+    contact_id = contacts_index.add_contact(Contact("Josh", "55555"))
+    contacts_index.remove_contact(contact_id)
+    print("After removal, search Josh", contacts_index.find_by_name("Josh"))
 
 if __name__ == "__main__":
     run_tests()
